000aa94400e200bb86ba361a2a8fe0913b2741e3
yuvraj2009bhatnagar <73655765+yuvraj2009bhatnagar@users.noreply.github.com>
2021-06-18 13:49:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,10 +12,10 @@ var question, contestant, quiz;
 function setup(){
   canvas = createCanvas(850,400);
   database = firebase.database();
-  quiz = new Quiz();
-  quiz.getState();1
-  quiz.start();
-  quiz.play();
+  //quiz = new Quiz();
+  //quiz.getState();1
+  //quiz.start();
+  //quiz.play();
 }
 
 
